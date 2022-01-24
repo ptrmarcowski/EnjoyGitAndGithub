390e988f23c301fae3b79645f3c7452941fa9c9d
Piotr Marcowski <piotr.marcowski@gmail.com>
2022-01-24 15:06:45 +0100

    modified file Anima.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,10 +1,26 @@
-public class Animal {
+public abstract class Animal {
 
     private int weight;
     private int height;
 
     public Animal(int weight, int height) {
-        this.weight = weight; 
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
         this.height = height;
     }
 }
