1ab04d7c93cca1e045fedcf1678d387835e26d9b
Piotr Marcowski <piotr.marcowski@gmail.com>
2022-01-24 15:21:31 +0100

    Account file

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+
+    private Username username;
+    private Password password;
+
+    public Account(Username username, Password password) {
+          this.username = username;
+        this.password = password;
+    }
+
+}
