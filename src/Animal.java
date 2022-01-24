3440a20ff8a45c2371af209fb04109738d3a600c
Piotr Marcowski <piotr.marcowski@gmail.com>
2022-01-24 15:04:46 +0100

    class Animal

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+
+    private int weight;
+    private int height;
+
+    public Animal(int weight, int height) {
+        this.weight = weight; 
+        this.height = height;
+    }
+}
